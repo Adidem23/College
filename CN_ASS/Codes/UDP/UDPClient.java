30959f41c73d135ac24f07353453e61d835f640e
Aditya Suryawanshi <124609794+Adidem23@users.noreply.github.com>
2023-10-31 18:17:40 +0530

    Update UDPClient.java

--- CN_ASS/Codes/UDP/UDPClient.java
+++ CN_ASS/Codes/UDP/UDPClient.java
@@ -1,1 +1,26 @@
+import java.io.IOException;
+import java.net.*;
+
+public class Receiver {
+    public static void main(String[] args) {
+        int receiverPort = 54321;
+
+        try (DatagramSocket socket = new DatagramSocket(receiverPort)) {
+            byte[] receiveData = new byte[1024];
+
+            while (true) {
+                DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
+                socket.receive(receivePacket);
+
+                String message = new String(receivePacket.getData(), 0, receivePacket.getLength());
+                InetAddress senderAddress = receivePacket.getAddress();
+                int senderPort = receivePacket.getPort();
+
+                System.out.println("Received data from " + senderAddress + ":" + senderPort + ": " + message);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
 
