f053d85b660992a10e3965bb27490c9cfa243e3b
Aditya Suryawanshi <124609794+Adidem23@users.noreply.github.com>
2023-10-31 18:16:58 +0530

    Update UDPServer.java

--- CN_ASS/Codes/UDP/UDPServer.java
+++ CN_ASS/Codes/UDP/UDPServer.java
@@ -1,1 +1,24 @@
+import java.io.IOException;
+import java.net.*;
+
+public class Server {
+    public static void main(String[] args) {
+        String receiverIP = "192.168.29.222";
+        int receiverPort = 54321;
+
+        try (DatagramSocket socket = new DatagramSocket()) {
+            InetAddress receiverAddress = InetAddress.getByName(receiverIP);
+
+            String message = "Aditya Suryawanshi is VIT Pune Student!!";
+            byte[] sendData = message.getBytes();
+
+            DatagramPacket packet = new DatagramPacket(sendData, sendData.length, receiverAddress, receiverPort);
+            socket.send(packet);
+
+            System.out.println("Message sent to receiver: " + message);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
 
