4a0bcf27086bf1ea8d7d6891fc02790edc42f6c8
Aditya Suryawanshi <124609794+Adidem23@users.noreply.github.com>
2023-11-26 22:58:11 +0530

    Update Ass7.java

--- AI_Labs/Ass7.java
+++ AI_Labs/Ass7.java
@@ -1,175 +1,120 @@
 import java.util.*;
 
+class Node {
+    int id;
+    int g;
+    int h;
+    Node parent;
+
+    Node(int id, int g, int h) {
+        this.id = id;
+        this.g = g;
+        this.h = h;
+        this.parent = null;
+    }
+}
+
+class Graph {
+    private Map<Integer, List<int[]>> graph = new HashMap<>();
+
+    void addEdge(int from, int to, int weight) {
+        if (!graph.containsKey(from)) {
+            graph.put(from, new ArrayList<>());
+        }
+        graph.get(from).add(new int[]{to, weight});
+    }
+
+    List<int[]> getNeighbors(int node) {
+        return graph.getOrDefault(node, Collections.emptyList());
+    }
+}
 
 public class AStar {
-    public static class Edge {
-        char source;
-        char destination;
-        int weight;
+    public static void main(String[] args) {
+
+        Graph graph = new Graph();
+
+        graph.addEdge(1,2,4);
+        graph.addEdge(1,3,2);
+        graph.addEdge(2,3,5);
+        graph.addEdge(2,4,10);
+        graph.addEdge(3,5,3);
+        graph.addEdge(5,4,4);
+        graph.addEdge(4,6,11);
 
 
-        public Edge(char s, char d, int w) {
-            this.source = s;
-            this.destination = d;
-            this.weight = w;
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter the starting node: ");
+        int startNode = scanner.nextInt();
+
+        System.out.print("Enter the destination node: ");
+        int goalNode = scanner.nextInt();
+
+
+
+        List<Integer> path = findShortestPath(graph, startNode, goalNode);
+
+        if (path != null) {
+            System.out.println("Shortest Path from " + startNode + " to " + goalNode + ": " + path);
+        } else {
+            System.out.println("No path found from " + startNode + " to " + goalNode);
         }
+
+        scanner.close();
     }
 
+    static List<Integer> findShortestPath(Graph graph, int startNode, int goalNode) {
 
-    public static class Node implements Comparable<Node> {
-        char label;
-        int cost; // g(n) + h(n)
-        int heuristic; // Heuristic value for this node
+        PriorityQueue<Node> openList = new PriorityQueue<>(Comparator.comparingInt(node -> node.g + node.h));
 
+        Map<Integer, Integer> gValues = new HashMap<>();
 
-        public Node(char l, int c, int h) {
-            this.label = l;
-            this.cost = c;
-            this.heuristic = h;
-        }
+        openList.add(new Node(startNode, 0, heuristic(startNode, goalNode)));
 
+        gValues.put(startNode, 0);
 
-        @Override
-        public int compareTo(Node other) {
-            return Integer.compare(this.cost, other.cost);
-        }
-    }
+        while (!openList.isEmpty()) {
+            Node currentNode = openList.poll();
 
-
-    public static void AStar(ArrayList<Edge>[] graph, int V, char start, char goal) {
-        PriorityQueue<Node> openSet = new PriorityQueue<>();
-        openSet.add(new Node(start, 0, heuristic(start, goal)));
-
-
-        int[] gScore = new int[V];
-        Arrays.fill(gScore, Integer.MAX_VALUE);
-        gScore[index(start)] = 0;
-
-
-        int[] cameFrom = new int[V];
-        Arrays.fill(cameFrom, -1);
-
-
-        while (!openSet.isEmpty()) {
-            Node current = openSet.poll();
-            char currentLabel = current.label;
-
-
-            if (currentLabel == goal) {
-                // Goal reached, reconstruct and print the path
-                reconstructPath(start, goal, cameFrom);
-                return;
+            if (currentNode.id == goalNode) {
+                return reconstructPath(currentNode);
             }
 
+            for (int[] neighborEdge : graph.getNeighbors(currentNode.id)) {
+                int neighborNode = neighborEdge[0];
+                int edgeWeight = neighborEdge[1];
+                int tentativeG = currentNode.g + edgeWeight;
+                int heuristicvalue= heuristic(neighborNode,goalNode);
 
-            for (Edge edge : graph[index(currentLabel)]) {
-                char neighbor = edge.destination;
-                int tentativeGScore = gScore[index(currentLabel)] + edge.weight;
-
-
-                if (tentativeGScore < gScore[index(neighbor)]) {
-                    // This is a better path
-                    cameFrom[index(neighbor)] = index(currentLabel);
-                    gScore[index(neighbor)] = tentativeGScore;
-                    openSet.add(new Node(neighbor, gScore[index(neighbor)] + heuristic(neighbor, goal), heuristic(neighbor, goal)));
+                if (!gValues.containsKey(neighborNode) || tentativeG < gValues.get(neighborNode)) {
+                    gValues.put(neighborNode, tentativeG);
+                    Node neighbor = new Node(neighborNode, tentativeG, heuristic(neighborNode, goalNode));
+                    neighbor.parent = currentNode;
+                    openList.add(neighbor);
                 }
             }
         }
 
-
-        System.out.println("No path found from " + start + " to " + goal);
+        return null;
     }
 
+    static List<Integer> reconstructPath(Node goalNode) {
+        List<Integer> path = new ArrayList<>();
+        Node current = goalNode;
 
-    public static int heuristic(char from, char to) {
-        // Assign heuristic values directly to nodes
-        int[] heuristicValues = {10,8,5,7,3,6,5,3,1,0}; // Corresponding to 'A' to 'J'
-        return heuristicValues[index(from)];
-    }
-
-
-    public static int index(char c) {
-        return c - 'A';
-    }
-
-
-    public static char label(int index) {
-        return (char) ('A' + index);
-    }
-
-
-    public static void reconstructPath(char start, char goal, int[] cameFrom) {
-        // 'cameFrom' array to backtrack from 'goal' to 'start'
-        // System.out.println("The Array cameFrom contains");
-        // System.out.println(Arrays.toString(cameFrom));
-        List<Character> path = new ArrayList<>();
-        char current = goal;
-        while (current != start) {
-            path.add(current);
-            current = label(cameFrom[index(current)]);
+        while (current != null) {
+            path.add(current.id);
+            current = current.parent;
         }
-        path.add(start);
+
         Collections.reverse(path);
-        System.out.println("Path from " + start + " to " + goal + ": " + path);
+
+        return path;
     }
 
-
-    public static void createGraph(ArrayList<Edge>[] graph) {
-        for (int i = 0; i < 10; i++) {
-            graph[i] = new ArrayList<Edge>();
-        }
-
-
-        graph[index('A')].add(new Edge('A', 'B', 6));
-        graph[index('A')].add(new Edge('A', 'F', 3));
-        graph[index('B')].add(new Edge('B', 'A', 6));
-        graph[index('B')].add(new Edge('B', 'D', 2));
-        graph[index('B')].add(new Edge('B', 'C', 3));
-        graph[index('C')].add(new Edge('C', 'B', 3));
-        graph[index('C')].add(new Edge('C', 'D', 1));
-        graph[index('C')].add(new Edge('C', 'E', 5));
-        graph[index('D')].add(new Edge('D', 'B', 2));
-        graph[index('D')].add(new Edge('D', 'C', 1));
-        graph[index('D')].add(new Edge('D', 'E', 8));
-        graph[index('E')].add(new Edge('E', 'C', 5));
-        graph[index('E')].add(new Edge('E', 'D', 8));
-        graph[index('E')].add(new Edge('E', 'I', 5));
-        graph[index('E')].add(new Edge('E', 'J', 5));
-        graph[index('J')].add(new Edge('J', 'E', 5));
-        graph[index('J')].add(new Edge('J', 'I', 3));
-        graph[index('I')].add(new Edge('I', 'E', 5));
-        graph[index('I')].add(new Edge('I', 'J', 3));
-        graph[index('I')].add(new Edge('I', 'G', 3));
-        graph[index('I')].add(new Edge('I', 'H', 2));
-        graph[index('G')].add(new Edge('G', 'I', 3));
-        graph[index('G')].add(new Edge('G', 'F', 1));
-        graph[index('F')].add(new Edge('F', 'A', 3));
-        graph[index('F')].add(new Edge('F', 'G', 1));
-        graph[index('F')].add(new Edge('F', 'H', 7));
-        graph[index('H')].add(new Edge('H', 'I', 2));
-        graph[index('H')].add(new Edge('H', 'F', 7));
-
-
-
-
-        //
-
-
-    }
-
-
-    public static void main(String[] args) {
-        //Scanner sc = new Scanner(System.in);
-        int V = 10;
-        ArrayList<Edge>[] graph = new ArrayList[V];
-        createGraph(graph);
-
-
-        char start = 'A';
-        char goal = 'J';
-
-
-        AStar(graph, V, start, goal);
+    static int heuristic(int node, int goalNode) {
+        return Math.abs(node - goalNode);
     }
 }
-
